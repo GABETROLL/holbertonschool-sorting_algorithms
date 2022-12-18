733a49aec4c85fc4f04004a108c1cc5fde158287
GABETROLL <gabriel7icplhappy@gmail.com>
2022-12-18 23:28:18 +0000

    fix betty

--- swap.c
+++ swap.c
@@ -43,6 +43,37 @@ int swap_in_array(int *array, size_t size, size_t a, size_t b)
  * ASSUMING THAT NODE A IS ONE NODE CLOSER TO THE HEAD
  * THAN NODE B; THAT NODE A COMES BEFORE NODE B.
  *
+ * The following code should do
+ * something like this this:
+ *
+ * (HIGHER INDEXES ARE ALSO DISPLAYED HIGHER HERE)
+ *
+ * next_p?         = &{3, node_b_p, ?}
+ * node_b_p        = &{2, node_a_p, next_p}
+ * node_a_p        = &{1, prev_p,   node_b_p}
+ * prev_p?         = &{0, ?,        node_a_p}
+ *
+ * (swap occurs)
+ *
+ * next_p?         = &{3, node_a_p, ?}
+ * node_b_p        = &{2, prev_p,   node_a_p}
+ * node_a_p        = &{1, node_b_p, next_p}
+ * prev_p?         = &{0, ?,        node_b_p}
+ *
+ * which is equivalent to:
+ *
+ * next_p?         = &{3, node_a_p, ?}
+ * node_a_p        = &{1, node_b_p, next_p}
+ * node_b_p        = &{2, prev_p,   node_a_p}
+ * prev_p?         = &{0, ?,        node_b_p}
+ *
+ * (Assuming that the names are pointers to
+ * structs in the heap)
+ *
+ * We also don't know if the next_p and prev_p
+ * pointers aren't NULL, so changing those
+ * pointers is inside two if statements below.
+ *
  * @node_a_pp: node WITH LOWER INDEX IN LIST
  * @node_b_pp: node WITH HIGHER INDEX IN LIST
  *
@@ -50,39 +81,6 @@ int swap_in_array(int *array, size_t size, size_t a, size_t b)
  */
 void swap_in_list(listint_t **node_a_pp, listint_t **node_b_pp)
 {
-	/*
-	 * The following code should do
-	 * something like this this:
-	 *
-	 * (HIGHER INDEXES ARE ALSO DISPLAYED HIGHER HERE)
-	 *
-	 * next_p?         = &{3, node_b_p, ?}
-	 * node_b_p        = &{2, node_a_p, next_p}
-	 * node_a_p        = &{1, prev_p,   node_b_p}
-	 * prev_p?         = &{0, ?,        node_a_p}
-	 *
-	 * (swap occurs)
-	 *
-	 * next_p?         = &{3, node_a_p, ?}
-	 * node_b_p        = &{2, prev_p,   node_a_p}
-	 * node_a_p        = &{1, node_b_p, next_p}
-	 * prev_p?         = &{0, ?,        node_b_p}
-	 *
-	 * which is equivalent to:
-	 *
-	 * next_p?         = &{3, node_a_p, ?}
-	 * node_a_p        = &{1, node_b_p, next_p}
-	 * node_b_p        = &{2, prev_p,   node_a_p}
-	 * prev_p?         = &{0, ?,        node_b_p}
-	 *
-	 * (Assuming that the names are pointers to
-	 * structs in the heap)
-	 *
-	 * We also don't know if the next_p and prev_p
-	 * pointers aren't NULL, so changing those
-	 * pointers is inside two if statements below.
-	 */
-
 	/* to ACCESS the NODES THEMSELVES in the heap */
 	listint_t *prev_p = (*node_a_pp)->prev;
 	listint_t *node_a_p = *node_a_pp;
