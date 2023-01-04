532033a20a2e3678cea14f9b8cf80533cc8ac05f
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-04 14:01:11 +0000

    Move swap functions to print files

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 Learn with me and Holberton about sorting algorithms and time complexity.
 ## Directory:
 All files that start with the same number talk about the same algorithm.
-The _swap.c_ file has the swap operation, which is used in multiple algorithms.
 The _print\_array.c_ and _print\_list_ just contain one function that just prints
-an array or a doubly linked list.
+an array or a doubly linked list, and a swap operation for the corresponding data type.
 Finally, the header file puts it all together. Enjoy!
 
--- print_array.c
+++ print_array.c
@@ -23,3 +23,42 @@ void print_array(const int *array, size_t size)
 	printf("\n");
 }
 
+/**
+ * swap_in_array - Tries to swap ints in array parameter
+ * 'array' that contains parameter 'size' ints.
+ *
+ * If the indexes are valid, it tries swapping
+ * and returns EXIT_SUCCESS if successful.
+ *
+ * If the indexes aren't valid, return EXIT_FAILURE.
+ *
+ * If the array is somehow not as big as 'size' says,
+ * undefined behaviour happens.
+ *
+ * @array: array to swap two ints in
+ * @size: size of the array measured in ints
+ * @a: index of int in array 'array' to swap
+ * with int in array 'array' at index 'b'
+ * @b: index of int in array 'array' to swap
+ * with int in array 'array' at index 'a'
+ *
+ * Return: EXIT_SUCCESS if indexes are valid,
+ * EXIT_FAILURE otherwise
+ */
+int swap_in_array(int *array, size_t size, size_t a, size_t b)
+{
+	int temp;
+
+	if (a >= size || b >= size)
+		return (1);
+
+	temp = array[a];
+	array[a] = array[b];
+	array[b] = temp;
+
+	return (0);
+}
+/*
+ * (it's here because the 'swap.c' file wouldn't be compiled
+ * by the Holberton test)
+ */
